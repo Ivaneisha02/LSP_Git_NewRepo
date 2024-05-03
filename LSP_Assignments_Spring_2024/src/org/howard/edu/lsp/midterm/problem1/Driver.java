1b99d4148eb74d627bc37e35c87231d64a181a86
Ivaneisha02 <90430746+Ivaneisha02@users.noreply.github.com>
2024-05-02 20:47:47 -0400

    Update Driver.java

--- LSP_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/problem1/Driver.java
+++ LSP_Assignments_Spring_2024/src/org/howard/edu/lsp/midterm/problem1/Driver.java
@@ -1,1 +1,11 @@
+package org.howard.edu.lsp.midterm.problem1;
+public class Driver {
 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(SecurityOps.encrypt("I love CSCI363" ));
+	}
+
+	
+	
+}
